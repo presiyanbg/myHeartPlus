3b9771f066d78114a7992bc7d7a923f222e13b2b
Presiyan Tsonevski <presianbgkz123@gmail.com>
2023-03-03 00:05:51 +0200

    API - Displaying medicaments

--- api/app/Models/Medicament.php
+++ api/app/Models/Medicament.php
@@ -21,4 +21,35 @@ class Medicament extends Model
         'description',
         'rating',
     ];
+
+    /**
+     * Display medicaments from array.
+     *
+     * @param  string array of connected medicaments 
+     * @return array App\Models\Medicament
+     */
+    public static function shotMedicaments($medicamentsIDs)
+    {
+        $medicamentsIDs = json_decode($medicamentsIDs);
+        $medicaments = [];
+
+        // Check if json is valid 
+        if (json_last_error() != 0 && is_array($medicamentsIDs)) {
+            return $medicaments;
+        }
+
+        foreach ($medicamentsIDs as $medId) {
+            // Load medicament 
+            $medicament = Medicament::where('id', $medId)->first();
+
+            // Load category 
+            $medicament->category = HealthCategory::where('id', $medicament->category_id)->first();
+
+            if ($medicament) {
+                array_push($medicaments, $medicament);
+            }
+        }
+
+        return $medicaments;
+    }
 }
