780fd70d87799a70df060443f7ef3a94f93f8deb
presiyanbg <presianbgkz123@gmail.com>
2023-12-26 23:03:17 +0200

    WEB-Next: Loading fix

--- web-next/src/components/loading/loading.tsx
+++ web-next/src/components/loading/loading.tsx
@@ -1,15 +1,9 @@
-'use client';
 import { Spinner } from "@nextui-org/react";
-import { useTranslations } from "next-intl";
 
 const Loading = () => {
-    const t = useTranslations();
-
     return (
         <div className="w-full flex flex-col items-center justify-center h-screen gap-4">
             <Spinner color="primary" />
-
-            <span className="text-foreground">{t('Loading')}...</span>
         </div>
     )
 }
