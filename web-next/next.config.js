61cd45fbb646ecae94e71c94c9e7703f84c969de
presiyanbg <presianbgkz123@gmail.com>
2024-01-12 09:01:25 +0200

    WEB-Next: Config change - Home page no cache

--- web-next/next.config.js
+++ web-next/next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: () => [
+        {
+            source: '/',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store',
+                },
+            ],
+        },
+    ],
     images: {
         remotePatterns: [
             {
