e927845ffb4cd06127593ad9923dc1eb815dac12
Presiyan Tsonevski <presianbgkz123@gmail.com>
2025-08-19 16:45:39 +0300

    WEB-Next: Added loader to auth portal

--- web-next/src/components/authentication/authenticationPortal/authenticationPortal.tsx
+++ web-next/src/components/authentication/authenticationPortal/authenticationPortal.tsx
@@ -2,18 +2,22 @@
 
 import PageTitle from "@/components/layouts/pageTitle/pageTitle";
 import { UserContext } from "@/context/userContext/userContextProvider";
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Divider, Spinner } from "@nextui-org/react";
 import { useTranslations } from "next-intl";
 import { useContext, useEffect, useState } from "react";
 import Logout from "../logout/logout";
 import Login from "../login/login";
 import Registration from "../registration/registration";
+import { LoadingContext } from "@/context/loadingContext/loadingContextProvider";
 
 
 const AuthenticationPortal = () => {
     const [display, setDisplay] = useState<'login' | 'register' | 'logout'>('login');
+    const [isLoaded, setIsLoaded] = useState<Boolean>(false);
 
     const { isAuth } = useContext(UserContext);
+    const { isLoading } = useContext(LoadingContext);
+
     const t = useTranslations();
 
     // Display login or register page
@@ -47,52 +51,65 @@ const AuthenticationPortal = () => {
         toggleDisplay();
     }, [isAuth]);
 
+    // Check loading
+    useEffect(() => {
+        setIsLoaded(!isLoading);
+    }, [isLoading]);
+
     return (
         <Card>
-            <CardHeader>
-                <PageTitle title={t(display)}></PageTitle>
-            </CardHeader>
+            {isLoaded &&
+                <>
+                    <CardHeader>
+                        <PageTitle title={t(display)}></PageTitle>
+                    </CardHeader>
 
-            <Divider />
+                    <Divider />
 
-            <CardBody>
-                <div className="d-flex flex-column justify-content-between h-100">
-                    {/* Logout page */}
-                    {
-                        isAuth && <Logout displayLogin={displayLogin}></Logout>
-                    }
+                    <CardBody>
+                        <div className="d-flex flex-column justify-content-between h-100">
+                            {/* Logout page */}
+                            {
+                                isAuth && <Logout displayLogin={displayLogin}></Logout>
+                            }
 
-                    {/* Login / Registration page */}
-                    {
-                        !isAuth && <>
-                            <div className="row justify-content-center mb-3">
-                                <div className="col-8">
-                                    {/* Login page */}
-                                    {display == 'login' && <Login></Login>}
+                            {/* Login / Registration page */}
+                            {
+                                !isAuth && <>
+                                    <div className="row justify-content-center mb-3">
+                                        <div className="col-8">
+                                            {/* Login page */}
+                                            {display == 'login' && <Login></Login>}
 
-                                    {/* Registration page */}
-                                    {display == 'register' && <Registration></Registration>}
-                                </div>
-                            </div>
+                                            {/* Registration page */}
+                                            {display == 'register' && <Registration></Registration>}
+                                        </div>
+                                    </div>
 
-                            <div className="w-full">
-                                <p className="cursor-pointer text-center" onClick={() => toggleDisplay()}>
-                                    {/* Login message */}
-                                    {display == 'register' && t('Already have a registration?') + ' '}
-                                    {display == 'register' && <span className="text-primary text--underline--hover">{t('Login here')}</span>}
+                                    <div className="w-full">
+                                        <p className="cursor-pointer text-center" onClick={() => toggleDisplay()}>
+                                            {/* Login message */}
+                                            {display == 'register' && t('Already have a registration?') + ' '}
+                                            {display == 'register' && <span className="text-primary text--underline--hover">{t('Login here')}</span>}
 
-                                    {/* Register message */}
-                                    {display == 'login' && t('New user?') + ' '}
-                                    {display == 'login' && <span className="text-primary text--underline--hover">{t('Register here')}</span>}
-                                </p>
-                            </div>
-                        </>
-                    }
-                </div>
-            </CardBody>
+                                            {/* Register message */}
+                                            {display == 'login' && t('New user?') + ' '}
+                                            {display == 'login' && <span className="text-primary text--underline--hover">{t('Register here')}</span>}
+                                        </p>
+                                    </div>
+                                </>
+                            }
+                        </div>
+                    </CardBody>
+                </>
+            }
+
+            {!isLoaded &&
+                <CardBody>
+                    <Spinner color="default" />
+                </CardBody>
+            }
         </Card>
-
-
     );
 }
 
