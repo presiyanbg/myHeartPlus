058103118a1fe06246635a9a988871b60c05b019
Presiyan Tsonevski <presianbgkz123@gmail.com>
2025-08-23 15:43:34 +0300

    WEB-Next: Health statistics

--- web-next/src/components/user/userComponents/userStatistics/userStatistics.tsx
+++ web-next/src/components/user/userComponents/userStatistics/userStatistics.tsx
@@ -1,5 +1,8 @@
 import { UserType } from "@/ts/types";
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import UserDailyStatistics from "../userDailyStatistics/userDailyStatistics";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSmile } from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
     user: UserType
@@ -7,17 +10,48 @@ type Props = {
 
 const UserStatistics = (props: Props) => {
 
-console.log(props.user)
+    console.log(props.user)
 
     return (
-        <Card>
-            <CardHeader>
-                <h4>Health statistics</h4>
-            </CardHeader>
-            
-            <CardBody>
-            </CardBody>
-        </Card>
+        <div className="flex flex-row">
+            <div className="w-2/3 pr-2">
+                <Card>
+                    <CardHeader>
+                        <h3>Health statistics</h3>
+                    </CardHeader>
+
+                    <CardBody>
+                        <div>
+                            <div className="text-center w-full pt-3 pb-3">
+                                <FontAwesomeIcon icon={faSmile} className="text-9xl pb-3 text-success-500" />
+
+                                <div className="w-full pt-1 pb-1">
+                                    <span>
+                                        General condition:
+                                    </span>
+
+                                    <span className="pl-2 font-bold text-success-500">
+                                        GOOD
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div>
+
+                            </div>
+                        </div>
+                    </CardBody>
+                </Card>
+            </div>
+
+            <div className="w-1/3">
+                <Card>
+                    <CardBody>
+                        <UserDailyStatistics></UserDailyStatistics>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
     )
 }
 
